8b408ce2f18e0445e9601d14ed7f8451719f3f92
benjaminneoh <benjaminneoh6343@gmail.com>
2024-10-01 10:44:05 +1000

    setup identity context

--- career-link-webapi/Data/CareerLinkDbContext.cs
+++ career-link-webapi/Data/CareerLinkDbContext.cs
@@ -1,54 +1,20 @@
 ﻿using Career_link_webapi.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Career_link_webapi.Data
 {
-    public class CareerLinkDbContext : DbContext
+    public class CareerLinkDbContext : IdentityDbContext<User>
     {
         public CareerLinkDbContext(DbContextOptions options) : base(options) { }
-
-        public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>().HasData(
-               new User
-               {
-                   UserId = 1,
-                   FirstName = "John",
-                   LastName = "Doe",
-                   EmailName = "john.doe@example.com",
-                   ContactNumber = 1234567890,
-                   CreatedDate = DateTime.Now,
-                   UpdatedDate = DateTime.Now
-               },
-               new User
-               {
-                   UserId = 2,
-                   FirstName = "Jane",
-                   LastName = "Smith",
-                   EmailName = "jane.smith@example.com",
-                   ContactNumber = 9876543210,
-                   CreatedDate = DateTime.Now,
-                   UpdatedDate = DateTime.Now
-               },
-               new User
-               {
-                   UserId = 3,
-                   FirstName = "Alice",
-                   LastName = "Johnson",
-                   EmailName = "alice.johnson@example.com",
-                   ContactNumber = 5551234567,
-                   CreatedDate = DateTime.Now,
-                   UpdatedDate = DateTime.Now
-               }
-           );
-
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Post>().HasData(
                 new Post
                 {
                     PostId = 1,
-                    UserId = 1,
                     Description = "Looking for a software engineering job.",
                     CreatedDate = DateTime.Now,
                     UpdatedDate = DateTime.Now
@@ -56,7 +22,6 @@ namespace Career_link_webapi.Data
                 new Post
                 {
                     PostId = 2,
-                    UserId = 2,
                     Description = "Excited about new opportunities in data science.",
                     CreatedDate = DateTime.Now,
                     UpdatedDate = DateTime.Now
@@ -64,7 +29,6 @@ namespace Career_link_webapi.Data
                 new Post
                 {
                     PostId = 3,
-                    UserId = 3,
                     Description = "Interested in remote work positions.",
                     CreatedDate = DateTime.Now,
                     UpdatedDate = DateTime.Now
@@ -73,3 +37,4 @@ namespace Career_link_webapi.Data
         }
     }
 }
+
