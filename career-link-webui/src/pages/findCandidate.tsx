51d8526b0ab77e44ee57cd96d57ecd15a8502d0e
Dipesh <dipesh.thapa@student.uts.edu.au>
2024-10-09 10:03:09 +1100

    Finished find a candidate page

--- career-link-webui/src/pages/findCandidate.tsx
+++ career-link-webui/src/pages/findCandidate.tsx
@@ -5,7 +5,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form,FormControl,FormField,FormItem, FormLabel, FormMessage } from "@/components/form";
 import { Input } from "@/components/input";
 import { Button } from "@/components/button";
-import { Select, SelectTrigger, SelectValue, SelectItem, SelectContent } from "@/components/select";
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import "react-quill/dist/quill.snow.css";
@@ -20,5 +19,93 @@ const formSchema = z
   })
 
 export default function FindCandidate(){
+  const form = useForm <z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues:{
+      jobtitle: "",
+      experience: "",
+      description:"",
+    },
+  });
+  const handleSubmit =()=>{
+    //GET OR POST METHOD
+  };
+  const router = useRouter();
+  const handleCancel=()=>{
+
+    router.push('/'); 
+  };
+
+  return(
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          FIND A CANDIDATE 
+          
+        </p>
+      <Form {...form}>
+        <form 
+          onSubmit={form.handleSubmit(handleSubmit)} 
+          className="max-w-md w-full flex-col gap-4"
+        >
+          <FormField 
+            control={form.control} 
+            name="jobtitle" 
+            render={({ field }) => {
+              return (
+              <FormItem>
+                <FormLabel className="text-black">Job Title</FormLabel>
+                
+                  <FormControl>
+                    <Input className="text-black" placeholder="Full Name" type="FullName" {...field}/>
+                  </FormControl>
+                
+                <FormMessage className="text-red-500"/>
+              </FormItem>
+            );
+            }}
+          />
+          <FormField 
+            control={form.control} 
+            name="experience" 
+            render={({ field }) => {
+              return (
+              <FormItem>
+                
+                <FormLabel className="text-black">Experience</FormLabel>
+                    <FormControl>
+                      <Input className="text-black" placeholder="Email" type ="email" {...field}/>
+                    </FormControl>      
+                <FormMessage className="text-red-500"/>
+              </FormItem>
+            );
+            }}
+          />
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-black">Bio</FormLabel>
+                <FormControl>
+                  <Input className="text-black" placeholder="Tell us about yourself" type ="jobskills" {...field}/>
+                </FormControl>
+                
+                <FormMessage className="text-red-500" />
+              </FormItem>
+            )}
+          />
+          <div className="flex space-x-4"> 
+            <Button type="submit" className="w-full md:w-auto">Submit</Button>
+            <Button type="button" className="w-full md:w-auto" onClick= {handleCancel}>
+              Cancel
+            </Button>
+          </div>
+         
+        </form>
+      </Form>
+            
+    </main>
     
+  )
 }
