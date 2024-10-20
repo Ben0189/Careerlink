9c57d34486e7e77006711c2492b6f67c1f8b5345
benjaminneoh <benjaminneoh6343@gmail.com>
2024-10-20 20:12:28 +1100

    update user column

--- career-link-webapi/Data/Entities/User.cs
+++ career-link-webapi/Data/Entities/User.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Identity;
 namespace Career_link_webapi.Data;
 public class User : IdentityUser
 {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime UpdatedDate { get; set; }
     public ICollection<Post> Posts { get; set; }
---
+++ career-link-webapi/Migrations/20241020091155_UpdateUserColumn.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Career_link_webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "testuser",
+                columns: new[] { "ConcurrencyStamp", "FirstName", "LastName", "SecurityStamp" },
+                values: new object[] { "9a2b11d6-3a89-4782-85bc-8b8258cf93b9", "TestFirstName", "TestLastName", "2d535fe1-66b1-4dcc-914c-eb7ccf69f4c9" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "testuser",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "201f588d-a5aa-4cf5-bb17-e55db39de7e2", "e5c8c1c9-623c-458d-82e8-fa5350310058" });
+        }
+    }
+}
