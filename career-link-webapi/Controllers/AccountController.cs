7930da7682700d24764744e6e6d1229eb523e49d
benjaminneoh <benjaminneoh6343@gmail.com>
2024-10-20 22:22:59 +1100

    custom login and register endpoint

---
+++ career-link-webapi/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+using Career_link_webapi.Data;
+using Career_link_webapi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Career_link_webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new User { 
+                    UserName = model.Email, 
+                    Email = model.Email, 
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    PhoneNumber = model.PhoneNumber,
+                    IsRecruiter = model.IsRecruiter
+                };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { user.Email });
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, true, false);
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    return Unauthorized();
+                }
+
+                var userData = new 
+                {
+                    user.UserName,
+                    user.Email,
+                    user.PhoneNumber,
+                    user.FirstName,
+                    user.LastName,
+                    user.IsRecruiter
+                };
+
+                return Ok(userData); // Return user data
+            }
+
+            return Unauthorized();
+        }
+
+    }
+}
